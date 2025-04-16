5dd8b7d2180c558970a114ffe9bbe21fc1bec916
Cristian Fioravanti <cristian.fioravanti@micegroup.it>
2025-04-16 18:32:18 +0200

    automigrazione con anessa creazione del db

--- database/databasesetup.go
+++ database/databasesetup.go
@@ -12,7 +12,19 @@ var Client *gorm.DB
 
 // DBSet stabilisce la connessione al database utilizzando GORM
 func DBSet() (*gorm.DB, error) {
-	// Dettagli di connessione MySQL
+	urlDBNoSchema := "root:root@tcp(localhost:3306)/"
+	dbNoSchema, err := gorm.Open(mysql.Open(urlDBNoSchema), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database server: %v", err)
+	}
+
+	// Crea il database "ecommerce" se non esiste
+	createDBQuery := "CREATE DATABASE IF NOT EXISTS ecommerce"
+	if err := dbNoSchema.Exec(createDBQuery).Error; err != nil {
+		return nil, fmt.Errorf("failed to create database: %v", err)
+	}
+
+	// Dettagli di connessione MySQL con il database "ecommerce"
 	urlDB := "root:root@tcp(localhost:3306)/ecommerce"
 	db, err := gorm.Open(mysql.Open(urlDB), &gorm.Config{})
 	if err != nil {
@@ -30,6 +42,11 @@ func DBSet() (*gorm.DB, error) {
 		return nil, fmt.Errorf("failed to ping database: %v", err)
 	}
 
+	// Crea le tabelle se non esistono già
+	err = db.AutoMigrate(&models.User{}, &models.Product{})
+	if err != nil {
+		fmt.Println("Errore durante la migrazione delle tabelle: %v", err)
+	}
 	fmt.Println("Successfully connected to MySQL database")
 	return db, nil
 }
@@ -42,20 +59,4 @@ func init() {
 		fmt.Println("Error connecting to database:", err)
 		// Gestisci l'errore (potresti voler eseguire un panic o uscire)
 	}
-}
-
-func UserData() ([]models.User, error) {
-	var users []models.User
-	if err := Client.Find(&users).Error; err != nil {
-		return nil, err
-	}
-	return users, nil
-}
-
-func ProductData() ([]models.Product, error) {
-	var products []models.Product
-	if err := Client.Find(&products).Error; err != nil {
-		return nil, err
-	}
-	return products, nil
 }
